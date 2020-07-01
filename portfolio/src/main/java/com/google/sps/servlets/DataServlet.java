7ae436b70da4cd341614c5211b3e2880fadf6d8a
leo <leo.galvez2452@gmail.com>
2020-07-01 00:35:14 +0000

    Implement JSON in /data servlet and display on page

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,7 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
@@ -38,9 +39,16 @@ public class DataServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String greeting = greetings.get((int) (Math.random() * greetings.size()));
+        String greeting = convertToGson(greetings);
 
-        response.setContentType("text/html;");
+        response.setContentType("application/json;");
         response.getWriter().println(greeting);
+    }
+
+    /*Converts an arrayList instance into a JSON string with Gson library.*/
+    private String convertToGson(List<String> greetings) {
+        Gson conversion = new Gson();
+        String result = conversion.toJson(greetings);
+        return result;
     }
 }
