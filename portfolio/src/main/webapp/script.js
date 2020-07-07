3be47d69e25172864187d6025a5cc207a25accb4
leo <leo.galvez2452@gmail.com>
2020-07-07 20:37:54 +0000

    Number.isInteger() doesn't seem reliable - works some days and not others. Reverted back to other implementation that seems more reliable.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -99,7 +99,7 @@ function questions() {
  */
 function updateComments() {
     var numComments = document.getElementById("num-comments").value;
-    if(numComments < 1 || !Number.isInteger(numComments)) {
+    if(numComments < 1 || (numComments - Number.parseInt(numComments)) !== 0) {
         alert("Please enter a positive integer.");
         return;
     }
