d00429e17dea2c9f267943f23012d6798906a387
leo <leo.galvez2452@gmail.com>
2020-07-21 15:40:25 +0000

    Add map onscreen

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -192,3 +192,10 @@ function createAElement(url) {
   aElement.innerText = "here";
   return aElement;
 }
+
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
